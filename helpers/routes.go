6ac4653025b49b1c04c8cb1eaa5a66182553f095
David McClure <dmcclure@vmware.com>
2020-08-27 17:39:55 +0000

    cf cli v7 no longer uses space for create route
    Signed-off-by: Amelia Downs <adowns@vmware.com>

--- helpers/routes.go
+++ helpers/routes.go
@@ -66,7 +66,7 @@ func CreateTcpRouteWithRandomPort(space, domain string, timeout time.Duration) u
 
 	var responseBuffer *gexec.Session
 	if isVersion7() {
-		responseBuffer = cf.Cf("create-route", space, domain)
+		responseBuffer = cf.Cf("create-route", domain)
 	} else {
 		responseBuffer = cf.Cf("create-route", space, domain, "--random-port")
 	}
